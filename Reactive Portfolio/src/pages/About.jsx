96f7fd002d56f1cd4ea77748eae1c1a29a854ada
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-12-06 19:18:44 -0600

    almost there

--- Reactive Portfolio/src/pages/About.jsx
+++ Reactive Portfolio/src/pages/About.jsx
@@ -7,24 +7,58 @@ export default function About() {
         <div className='profile'>
         <img className="image" src={SichounImg} alt='SichounIMG'></img>
         <p className="infoscript">
-          Hello! My name is Sichoun Nplhaib Lee. I'm a Junior Web Developer, who is willing to do anything to learn a little more of everything.
+          <h3 className='subTitle'>
+            Sichoun Nplhaib Lee
+          </h3>
+          <p className="discript">
+            Hello! My name is Sichoun Nplhaib Lee. I'm a Junior Web Developer, who is willing to do anything to learn a little more about everything.
+          </p>
+          <p className="discript">
+            "A Jack Of All Trades Is A Master Of None, But Better Then A Master of One"
+          </p>
+          <p className="discript">
+            "We Will Cross That Bridge, When We Cross That Bridge"
+          </p>
+          <p className="discript">
+            "If There Is A Will, There Is A Way"
+          </p>
+          <p className="discript">
+            "Don't Worry, Because When You Worry, It Makes Problems Double"
+          </p>
+          <p className="discript">
+            "All Is Well, Goes Well"
+          </p>
+          <p className="discript">
+            "Remember The Good, Forget The Bad"
+          </p>
         </p>
         </div>
         <p className="discript">
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+          Ever since I was a child, I had always found that I don't seem to fit in, so instead of trying to fit in, I tried to find myself and who I am. I had always loved math and science, so I knew I wanted to do something with either math or science or both.
         </p>
         <p className="discript">
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+          When I first played minecraft, I had always had a thing for building and making things. I would design elaborate buildings. It wasn't until my sister had told me that I should become an architect that really opened my eyes to what I wanted to do.
+        </p>
+        <p className="discript">
+          After a year of going to college for Architecture, and having a heartbreaking event, I had a life crises. Living day by day with out any passion or dream. I decided to find a job and work on myself. During that time I was working at Chipotle.
+        </p>
+        <p className="discript">
+          After a year of working at Chipotle, I had learned every position there and had driven myself bored or had burned myself out. I was at a standstill and had no idea what I really wanted to do. After awhile, my brother-in-law had asked me what I wanted to do, and he sugested coding.
+        </p>
+        <p className="discript">
+          I had done a little of coding in the past, but never persued it as a career. When my brother-in-law suggested persueing it, it opened my eyes and I found another step that I could take to the top.
+        </p>
+        <p className="discript">
+          "It's Not About How Many Times You Fall, It's About How Many Times You Get Back Up"
+        </p>
+        <p className="discript">
+          "The Only Way To Go When You Are At Rock Bottem Is Up"
+        </p>
+        <p className="discript">
+          "To Each Their Own"
+        </p>
+        <p className="discript">
+          "With Great Power Comes Great Responsability"
         </p>
       </div>
     );
--- Reactive Portfolio/src/pages/Portfolio.jsx
+++ Reactive Portfolio/src/pages/Portfolio.jsx
@@ -2,6 +2,9 @@ export default function Portfolio() {
     return (
       <div className="contentCard">
         <h2 className="title">Portfolio Page</h2>
+        <h3 className="subTitle">
+          Project Name
+        </h3>
         <p className="discript">
           Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
           Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
